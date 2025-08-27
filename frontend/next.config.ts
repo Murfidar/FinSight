1de5f3312fb0a10486514b0d00467d0d4331b35f
Murfidar <murfidar@gmail.com>
2025-08-27 01:44:30 +0000

    chore: Fix outputFileTracingRoot in next.config.ts (frontend)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import type { NextConfig } from 'next'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingRoot: __dirname
 }
 
 export default nextConfig
